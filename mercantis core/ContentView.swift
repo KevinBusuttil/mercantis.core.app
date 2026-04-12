ac41d60693c3ba52a4a79751481dc5c288b5bb2f
Kevin Busuttil <kevin@busuttil-technologies.com>
2026-04-12 23:15:50 +0200

    Initial Commit

---
+++ mercantis core/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mercantis core
+//
+//  Created by Kevin Busuttil on 12/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mercantis core/mercantis_coreApp.swift
@@ -0,0 +1,17 @@
+//
+//  mercantis_coreApp.swift
+//  mercantis core
+//
+//  Created by Kevin Busuttil on 12/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct mercantis_coreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
